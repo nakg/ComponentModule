2d07f26bfa426c999cf0409a3b32173126c9dd0b
김낙진 <nakjin@mail.shinhan.dev>
2022-01-26 01:06:47 +0900

    Developing GMDropdown_DebbitAccount.. ing..

--- ComponentModule/View/GMDropdown_DebitAccount.swift
+++ ComponentModule/View/GMDropdown_DebitAccount.swift
@@ -8,15 +8,53 @@
 import SwiftUI
 
 public struct GMDropdown_DebitAccount: View {
+	@EnvironmentObject var store: DebitAccountStore
+	
 	public init() {
 		
 	}
 	
 	public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+		Button(action: {
+			
+		}, label: {
+			VStack {
+				LineView(height: .constant(1.0))
+				Group {
+					VStack (spacing: 4.0) {
+						VStack(spacing: 7.0) {
+							HStack {
+								Text("test")
+								Image("iconArrowDown")
+							}
+							
+							Text("test")
+						}
+						VStack(spacing: 4.0) {
+							Text("test")
+							Text("test")
+						}
+					}
+				}
+				.padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
+				LineView(height: .constant(5.0))
+			}
+		})
+	}
 }
 
+// MARK: Line View
+struct LineView: View {
+	@Binding public var height:CGFloat
+	var bottomTumbLineColor = Color(UIColor.systemRed)
+	var body: some View {
+		Rectangle()
+			.foregroundColor(bottomTumbLineColor)
+			.frame(height: height)
+	}
+}
+
+// MARK: - Preview
 struct GMDropdown_DebitAccount_Previews: PreviewProvider {
     static var previews: some View {
         GMDropdown_DebitAccount()
