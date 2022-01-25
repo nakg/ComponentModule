7147984965f356713d5c42c8a9dbd217d2ff2183
김낙진 <nakjin@mail.shinhan.dev>
2022-01-25 21:40:57 +0900

    Add Component View

---
+++ ComponentModule/View/GMDropdown_DebitAccount.swift
@@ -0,0 +1,20 @@
+//
+//  GMDropdown_DebitAccount.swift
+//  ComponentModule
+//
+//  Created by 60067670 on 2022/01/25.
+//
+
+import SwiftUI
+
+struct GMDropdown_DebitAccount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GMDropdown_DebitAccount_Previews: PreviewProvider {
+    static var previews: some View {
+        GMDropdown_DebitAccount()
+    }
+}
