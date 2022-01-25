45b2a00f269fc7048770f8f86da0deba8d25774f
김낙진 <nakjin@mail.shinhan.dev>
2022-01-25 20:32:09 +0900

    Initial Commit

---
+++ ComponentModule/ComponentModule.h
@@ -0,0 +1,18 @@
+//
+//  ComponentModule.h
+//  ComponentModule
+//
+//  Created by 60067670 on 2022/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComponentModule.
+FOUNDATION_EXPORT double ComponentModuleVersionNumber;
+
+//! Project version string for ComponentModule.
+FOUNDATION_EXPORT const unsigned char ComponentModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComponentModule/PublicHeader.h>
+
+
